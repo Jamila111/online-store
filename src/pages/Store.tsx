3d9776192b1d07934d59a49f899e5630662cddde
Jamila111 <163223712+Jamila111@users.noreply.github.com>
2025-02-04 00:01:25 -0500

    Update Store.tsx

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -3,7 +3,7 @@ import storeItems from "../data/items.json";
 import { StoreItem } from "../components/StoreItem.tsx";
 import { useShoppingCart } from "../context/ShoppingCartContext.tsx";
 import { useEffect, useMemo } from "react";
-import { Link, useSearchParams, useNavigate } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
 
 export function Store() {
     const [searchParams] = useSearchParams();
@@ -46,14 +46,7 @@ export function Store() {
                 <Row xs={1} sm={1} md={2} lg={3} className="g-3">
                     {filteredItems.map((item) => (
                         <Col key={item.id}>
-                            <Link
-                                to={`/store/product/${item.id}`}
-                                style={{textDecoration: 'none'}}
-                                onClick={() => handleItemClick(item.id)}
-                                className="d-block h-100"
-                            >
-                                <StoreItem {...item} />
-                            </Link>
+                            <StoreItem {...item} onImageClick={() => handleItemClick(item.id)} />
                         </Col>
                     ))}
                 </Row>
